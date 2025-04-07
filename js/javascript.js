6e48c726fc0c065cfbdaeb9558622ae52b1a34e1
GCS-DG <128108731+GCS-DG@users.noreply.github.com>
2025-04-06 20:29:10 -0500

    Add function for CPU player's choice

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+var comChoice
+var randNum
+
+// Function for computer player to choose rock, paper, or scissors
+function getComputerChoice(comChoice) {
+    randNum = Math.floor(Math.random() * 100 + 1);
+    switch(true) {
+        case randNum > 66:
+            comChoice = "Rock";
+            break;
+        case randNum > 33:
+            comChoice = "Paper";
+            break;
+        case randNum >= 0:
+            comChoice = "Scissors";
+    }
+    
+    return comChoice;
+  }
+console.log(getComputerChoice(comChoice))
