a467a1b696e2b1a66e98fdca11bb5d4aba863c08
GCS-DG <128108731+GCS-DG@users.noreply.github.com>
2025-04-06 22:54:24 -0500

    Add function for user choice and play 5 rounds.

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,91 @@
-var comChoice
-var randNum
+let humanScore = 0;
+let computerScore = 0;
 
 // Function for computer player to choose rock, paper, or scissors
-function getComputerChoice(comChoice) {
-    randNum = Math.floor(Math.random() * 100 + 1);
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random() * 100 + 1);
     switch(true) {
         case randNum > 66:
-            comChoice = "Rock";
+            choice = "rock";
             break;
         case randNum > 33:
-            comChoice = "Paper";
+            choice = "paper";
             break;
         case randNum >= 0:
-            comChoice = "Scissors";
-    }
-    
-    return comChoice;
-  }
-console.log(getComputerChoice(comChoice))
+            choice = "scissors";
+    };
+    return choice;
+}
+
+// Function to ask human player to choose rock, paper, or scissors
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper or scissors.").toLowerCase()
+    return choice;
+}
+
+function playRound() {
+    var roundResult
+    humanChoice = getHumanChoice()
+    computerChoice = getComputerChoice()
+    switch(true) {
+        case humanChoice === "rock":
+            if (computerChoice === "rock") {
+                roundResult = "You both chose rock. It is a tie.";
+            } else {
+                if (computerChoice === "paper") {
+                    roundResult = "You chose rock and the computer chose paper. You lose.";
+                    computerScore++;
+                } else {
+                    roundResult = "You chose rock and the computer chose scissors. You win!"
+                    humanScore++;
+                }
+            };
+            break;
+        case humanChoice === "paper":
+            if (computerChoice === "paper") {
+                roundResult = "You both chose paper. It is a tie.";
+            } else {
+                if (computerChoice === "scissors") {
+                    roundResult = "You chose paper and the computer chose scissors. You lose";
+                    computerScore++;
+                } else {
+                    roundResult = "You chose paper and the computer chose rock. You win!"
+                    humanScore++;
+                }
+            };
+            break;
+        case humanChoice === "scissors":
+            if (computerChoice === "scissors") {
+                roundResult = "You both chose scissors. It is a tie.";
+            } else {
+                if (computerChoice === "rock") {
+                    roundResult = "You chose scissors and the computer chose rock. You lose";
+                    computerScore++;
+                } else {
+                    roundResult = "You chose scissors and the computer chose paper. You win!"
+                    humanScore++;
+                }
+            };
+    };
+    console.log(roundResult)
+    console.log("You: " + humanScore + ", CPU: " + computerScore)
+}
+
+function playGame() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    if (humanScore > computerScore) {
+        console.log("You won the match!")
+    } else {
+        if (humanScore = computerScore) {
+            console.log("The match was a tie.")
+        } else {
+            console.log("You lost the match.")
+        }
+    };
+}
+
+playGame()
